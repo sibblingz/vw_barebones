3b9d72989722089c6cf1950933133300a9a5a93b
Benjamin Savage <ben@sibblingz.com>
2009-01-29 00:59:18 -0800

    changed some spaces to tabs in the model base generator

--- server/rails_app/lib/generators/model_base/templates/model_base.rb
+++ server/rails_app/lib/generators/model_base/templates/model_base.rb
@@ -6,7 +6,7 @@ package vw.app.models
 	{
 <%- shown_columns = class_name.constantize.columns.reject{|c| c.name == 'id'} -%>
 <%- shown_columns.each do |col| -%>
-	  public var <%= col.name -%>:<%= typify( col.type.to_s ) %>
+		public var <%= col.name -%>:<%= typify( col.type.to_s ) %>
 <%- end -%>
 	  
 		public function <%= class_name -%>Model(attributes:Object)
