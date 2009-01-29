16a4b7aa8a765a48b370a5af000b4e2d1616c07a
Benjamin Savage <ben@sibblingz.com>
2009-01-29 00:57:07 -0800

    git rid of extra spaces in model base file that get generated

--- server/rails_app/lib/generators/model_base/templates/model_base.rb
+++ server/rails_app/lib/generators/model_base/templates/model_base.rb
@@ -4,10 +4,10 @@ package vw.app.models
 
 	public class <%= class_name -%>Model extends ModelBase
 	{
-	        <% shown_columns = class_name.constantize.columns.reject{|c| c.name == 'id'} %>
-	        <% shown_columns.each do |col| %>
-	        public var <%= col.name -%>:<%= typify( col.type.to_s ) -%>
-	        <% end %>
+<%- shown_columns = class_name.constantize.columns.reject{|c| c.name == 'id'} -%>
+<%- shown_columns.each do |col| -%>
+	  public var <%= col.name -%>:<%= typify( col.type.to_s ) %>
+<%- end -%>
 	  
 		public function <%= class_name -%>Model(attributes:Object)
 		{
