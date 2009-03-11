a4fac1a0916e480e1861bc85cf08c23ef56edf8b
Daniel Jacobs <djacobs7@manowar.local>
2009-03-10 23:41:31 -0700

    removed things that are now in the extensions

--- server/rails_app/lib/if_platform.rb
+++
@@ -1,9 +0,0 @@
-class Object
-  def if_platform(platforms)
-    yield if [*platforms].include? PLATFORM_ENV
-  end
-
-  def unless_platform(platforms)
-    yield unless [*platforms].include? PLATFORM_ENV
-  end
-end
